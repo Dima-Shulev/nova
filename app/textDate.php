2aafa28653c7bbea42ac12e87581bafe11d3d463
Dima-Shulev <116750529+Dima-Shulev@users.noreply.github.com>
2022-12-29 12:28:27 +0300

    Text DB

---
+++ app/textDate.php
@@ -0,0 +1,17 @@
+<?php
+include_once "Connect.php";
+$connect = new PDO("mysql:host=".Connect::HOST.";dbname=".Connect::DB.";charset=".Connect::CHARSET,Connect::USER,Connect::PASS);
+$result = [];
+function queryDateBase($connect){
+    $arr = $_POST;
+    $prepare = $connect->prepare("SELECT * FROM `test` WHERE id > '1'");
+    $prepare->execute();
+    $res = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    foreach($res as $value){
+        $result[] = $value;
+    }
+    $result = array_merge($result, $arr);
+    return $result;
+}
+var_dump(queryDateBase($connect));
+?>
