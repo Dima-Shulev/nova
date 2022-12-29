3c57bcabaf77583bf5014dde0467f40cd2921d2b
Dima-Shulev <116750529+Dima-Shulev@users.noreply.github.com>
2022-12-29 12:25:54 +0300

    Update app.php

--- app/app.php
+++ app/app.php
@@ -1,4 +1,20 @@
 <?php
+include_once "Connect.php";
+try {
+    $connect = new PDO("mysql:host=".Connect::HOST.";dbname=".Connect::DB.";charset=".Connect::CHARSET,Connect::USER,Connect::PASS);
+    if(isset($_POST)){
+       $arr = [];
+       $arr = $_POST;
+       $arr["birthday"] = date("Y-m-d",strtotime($arr["birthday"]));
+       $arr["date"] = date("Y-m-d H:m:s");
+       $arr["ip"] = $_SERVER["REMOTE_ADDR"];
+            $query = $connect->prepare("INSERT INTO `test` (`name`,`middle`,`birthday`,`date`,`ip`)VALUES(:nam,:middle,:birthday,:dates,:ip)");
+            $query->execute(['nam' => $arr["name"], 'middle' => $arr["middle"], 'birthday' => $arr["birthday"], 'dates' => $arr["date"], 'ip' => $arr["ip"]]);
+            $result = $query->fetch(PDO::FETCH_ASSOC);
 
-//var_dump($_POST);
-phpinfo();
+     }
+}catch(PDOException $e){
+    echo "Ошибка соединения с DB".$e->getMessage();
+}
+var_dump($arr);
+
