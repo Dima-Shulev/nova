53b0da6ac76cde419dc95173dcc549d3ad11528c
Dima-Shulev <116750529+Dima-Shulev@users.noreply.github.com>
2022-12-30 17:54:22 +0300

    Update textDate.php

--- app/textDate.php
+++ app/textDate.php
@@ -1,17 +1,20 @@
 <?php
 include_once "Connect.php";
 $connect = new PDO("mysql:host=".Connect::HOST.";dbname=".Connect::DB.";charset=".Connect::CHARSET,Connect::USER,Connect::PASS);
-$result = [];
+
 function queryDateBase($connect){
+    $result = [];
     $arr = $_POST;
     $prepare = $connect->prepare("SELECT * FROM `test` WHERE id > '1'");
     $prepare->execute();
     $res = $prepare->fetchAll(PDO::FETCH_ASSOC);
-    foreach($res as $value){
+
+    foreach($res as $value) {
         $result[] = $value;
     }
     $result = array_merge($result, $arr);
     return $result;
 }
+
 var_dump(queryDateBase($connect));
 ?>
