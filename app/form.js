5027b9e3722d1f88583e2d9c4ee5ed459aa55bf2
Dima-Shulev <116750529+Dima-Shulev@users.noreply.github.com>
2023-01-01 15:35:39 +0300

    Update form.js

--- app/form.js
+++ app/form.js
@@ -4,10 +4,9 @@ class Form{
     urlPhp;
     values;
     body;
-    fields;    
+    fields;
     table;
-    tr;   
-
+    tr;
 
     constructor(){
         this.form = document.querySelector(".form");
@@ -15,22 +14,18 @@ class Form{
         this.urlPhp = "app.php";
     }
 
-    ajax(ContentType,url,bodyResponse=null){
-        async function getDate(ContentType,bodyResponse=null){
-            let response = await fetch(url,{
+    ajax(contentType,url,bodyRes= null){
+           fetch(url,{
                 method: "POST",
                 headers: {
-                  "Content-Type": ContentType
+                    "Content-Type": contentType
                 },
-                body: bodyResponse
-            });
-            let result = await response.json();
-            console.log(result);
-        }
+                body: bodyRes
+            })
+                .then(response => response.json())
+                .then(result => this.tableCreat(result))
 
-        console.log(getDate(ContentType,bodyResponse));
     }
-
 
     responseCallBack(event){
         this.fields = document.querySelectorAll("input");
@@ -48,36 +43,30 @@ class Form{
         }
         this.body = this.body.join("&");
         this.ajax("application/x-www-form-urlencoded","app.php",this.body);
+
     }
 
+    CallBack(){
+        this.ajax("application/x-www-form-urlencoded","textDate.php");
+
+    }
     tableCreat(bodyArray){
-        this.table = document.createElement("table");
-        this.table.className = "myTable";
-        document.body.append(this.table);
-        this.tr = document.createElement("tr");
-        for(let i = 0; i < bodyArray.length; i++){
-            this.td = document.createElement("td");
-            this.td.innerText = bodyArray[i];
-            this.tr.append(this.td);
-        }
-        this.table.append(this.tr);
-    }
+       this.table = document.getElementById("table");
+            for (let key in bodyArray) {
+                this.tr = document.createElement("tr");
+                for (let value in bodyArray[key]) {
+                    this.td = document.createElement("td");
+                    this.td.innerText = bodyArray[key][value];
+                    this.tr.append(this.td);
+                    this.table.append(this.tr);
+                }
+           }
 
-    CallBack(event){
-        this.ajax("application/x-www-form-urlencoded","textDate.php",this.body);
-        this.tableCreat(this.body);
     }
-
 
     responseData(){
     this.result.addEventListener("click", () =>{
         this.responseCallBack();
     });
-    }
-
-    newResponse(){
-        this.result.addEventListener("click", () =>{
-            this.CallBack();
-        });
     }
 }
