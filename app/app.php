4b869589abc38495b4aadf898391d402f0b2916a
Dima-Shulev <116750529+Dima-Shulev@users.noreply.github.com>
2023-01-01 15:35:07 +0300

    Update app.php

--- app/app.php
+++ app/app.php
@@ -1,20 +1,22 @@
+
 <?php
 include_once "Connect.php";
 try {
     $connect = new PDO("mysql:host=".Connect::HOST.";dbname=".Connect::DB.";charset=".Connect::CHARSET,Connect::USER,Connect::PASS);
     if(isset($_POST)){
-       $arr = [];
-       $arr = $_POST;
-       $arr["birthday"] = date("Y-m-d",strtotime($arr["birthday"]));
-       $arr["date"] = date("Y-m-d H:m:s");
-       $arr["ip"] = $_SERVER["REMOTE_ADDR"];
-            $query = $connect->prepare("INSERT INTO `test` (`name`,`middle`,`birthday`,`date`,`ip`)VALUES(:nam,:middle,:birthday,:dates,:ip)");
-            $query->execute(['nam' => $arr["name"], 'middle' => $arr["middle"], 'birthday' => $arr["birthday"], 'dates' => $arr["date"], 'ip' => $arr["ip"]]);
-            $result = $query->fetch(PDO::FETCH_ASSOC);
-
-     }
+            $arr = [];
+            $arr = $_POST;
+            $arr["birthday"] = date("Y-m-d", strtotime($arr["birthday"]));
+            $arr["date"] = date("Y-m-d H:m:s");
+            $arr["ip"] = $_SERVER["REMOTE_ADDR"];
+             if((!empty($arr["name"])) && (!empty($arr["middle"])) && (!empty($arr["birthday"]))){
+               $query = $connect->prepare("INSERT INTO `test` (`name`,`middle`,`birthday`,`date`,`ip`)VALUES(:nam,:middle,:birthday,:dates,:ip)");
+               $query->execute(['nam' => $arr["name"], 'middle' => $arr["middle"], 'birthday' => $arr["birthday"], 'dates' => $arr["date"], 'ip' => $arr["ip"]]);
+               $result = $query->fetch(PDO::FETCH_ASSOC);
+               echo json_encode($arr);
+             }
+    }
 }catch(PDOException $e){
     echo "Ошибка соединения с DB".$e->getMessage();
 }
-var_dump($arr);
-
+?>
